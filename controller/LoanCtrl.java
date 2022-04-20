c26cbcf2bf62607cb6404fb99d341e9b33ee7d7a
LasseTN <103989770+LasseTN@users.noreply.github.com>
2022-04-20 10:38:15 +0200

    sync1

--- controller/LoanCtrl.java
+++ controller/LoanCtrl.java
@@ -1,35 +1,12 @@
 package controller;
+import model.*;
 
-
-/**
- * Write a description of class LoanCtrl here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class LoanCtrl
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class LoanCtrl
-     */
-    public LoanCtrl()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class LoanCtrl {
+    private PersonCtrl personCtrl;
+    private LPCtrl lpCtrl;
+    private Loan loan;
+    
+    public LoanCtrl() {
+        
     }
 }
