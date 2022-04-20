dcba62c4dd354e69f51b9ee5841853e1cf2c5592
MagnusPC <10434658@ucn.dk>
2022-04-20 12:15:34 +0200

    Update Loan.java

--- model/Loan.java
+++ model/Loan.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.ArrayList;
 
 
 /**
@@ -10,7 +11,8 @@ package model;
 public class Loan
 {
     // instance variables - replace the example below with your own
-    
+    private Person person;
+    private Lp lp;
 
     /**
      * Constructor for objects of class Loan
@@ -18,10 +20,18 @@ public class Loan
     public Loan()
     {
         // initialise instance variables
-        
+        person = null;
+        lp = null;
     }
-    //public Boolean addPerson(){
-        
-    //}
+
+    /*
+     * sørg for der ikke kan overskrives og person ikke er null
+     */
+    public void setPerson(Person person){
+        this.person = person;
+    }
     
+    public void setLp(Lp lp){
+        this.lp = lp;
+    }
 }
