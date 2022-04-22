9d934513a01bbb8dd3d0f2fc1876e377c499d551
MagnusPC <10434658@ucn.dk>
2022-04-22 11:17:29 +0200

    mere til TryMe

--- controller/TryMe.java
+++ controller/TryMe.java
@@ -16,9 +16,11 @@ import org.junit.jupiter.api.Test;
 public class TryMe
 {
     private controller.LoanCtrl loanCtrl1;
+    private model.Loan loan1;
     private model.Person person1;
     private model.LP lp1;
     private model.Copy copy1;
+    private model.Copy copy2;
 
     /**
      * Default constructor for test class TryMe
@@ -27,10 +29,12 @@ public class TryMe
     {
        //TODO to af hvers
        loanCtrl1 = new controller.LoanCtrl();
+       loan1 = new model.Loan(person1, copy1);
        person1 = new model.Person("bob", "vej 3", 9000, "bykær", "1234");
        //person2 = new model.Person("bente", "gade 8", 8000, "metropolis", "9999");
        lp1 = new model.LP(888, "artist1", "album1", 2010);
        copy1 = new model.Copy(001, "fjong", 99.99);
+       copy2 = new model.Copy(002, "ringe", 14.95);
     }
 
     /**
@@ -44,7 +48,9 @@ public class TryMe
         // model.Person p = loanCtrl1.findPersonByPhone("1234");
         // model.Copy c = loanCtrl1.findCopyBySerialNo(001);
         lp1.addCopy(copy1);
+        lp1.addCopy(copy2);
         loanCtrl1.createLoan(person1, copy1);
+        loan1.addCopy(copy2);
     }
 
     /**
